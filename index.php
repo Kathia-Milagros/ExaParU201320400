6b095d996a4cb664ad3a7d78c64f606eb740922d
Kathia Milagros Quico Cauti <u201320400@upc.edu.pe>
2017-05-10 17:41:05 +0000

    GIT COMMIT

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
+<<<<<<< Upstream, based on 8b1442b7e8f3a730ed5159d9b8d4df3b2c6ba214
 <title>PC1</title>
+=======
+<title>Parcial</title>
+>>>>>>> 242ba6b GIT COMMIT
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -22,6 +26,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
   <h1 class="w3-margin w3-jumbo">Kathia Milagros Quico Cauti 
   </h1>
   
+<<<<<<< Upstream, based on 8b1442b7e8f3a730ed5159d9b8d4df3b2c6ba214
   <h1 class="w3-margin w3-jumbo"> CLOUD COMPUTING - UPC</h1>
  
  
@@ -98,6 +103,84 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
      	<div>
      	<center> 
 		<image src="https://raw.githubusercontent.com/Kathia-Milagros/ExamenParcial/master/images/12.png" width="1000" height="400" />
+=======
+  <h1 class="w3-margin w3-jumbo"> CLOUD COMPUTING - UPC - PARCIAL</h1>
+ 
+ 
+</header>
+
+
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+     
+     	<div>
+     	<center> 
+		<image src="images/13.PNG"  width="1000" height="400" />
+	</center>
+	</div>	
+		
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    
+    <div class="w3-content">
+     <center>
+      <h1> LISTA DE SERVICIO </h1>
+      <table  style= "background-color:white" class ="table table-striped">
+   <?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b74ba3320e82ec";
+  $password = "2d194843";
+  $dbname = "ad_bb6ddb2f34daf52";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * from servicios";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {?>
+      <tr>
+      <td><?php echo $row['nombre_servicio']?></td>
+      <td><?php echo $row['descripcion_servicio']?></td>
+      <td><img src ="<?php echo $row['imagen_servicio']?>"></td>
+      </tr>
+     <?php }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+ ?> 
+ </table>
+      	<div>
+     	<center> 		
+		</center> 
+		</div>	
+	   </center>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+     
+     	<div>
+     	<center> 
+		<image src="images/12.PNG" width="1000" height="400" />
+>>>>>>> 242ba6b GIT COMMIT
 		</center>
 		</div>		
     </div>
