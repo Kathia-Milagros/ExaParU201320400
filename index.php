c8b64371abf1fbe1c0a04ced2a85e4595fb181fb
Kathia-Milagros <u201320400@upc.edu.pe>
2017-05-10 11:40:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,124 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
+<title>PC1</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:100px}
+</style>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+
+<!-- Header -->
+<header class="w3-container w3-red w3-center" style="padding:80px 10px">
+  <h1 class="w3-margin w3-jumbo">Kathia Milagros Quico Cauti 
+  </h1>
+  
+  <h1 class="w3-margin w3-jumbo"> CLOUD COMPUTING - UPC</h1>
+ 
+ 
+</header>
+
+
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+     
+     	<div>
+     	<center> 
+		<image src="https://raw.githubusercontent.com/Kathia-Milagros/ExamenParcial/master/images/13.png"  width="1000" height="400" >
+		</center> 
+		</div>		
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    
+    <div class="w3-content">
+     <center>
+      <h1> LISTA DE SERVICIO </h1>
+      <table  style= "background-color:white" class ="table table-striped">
+   <?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b74ba3320e82ec";
+  $password = "2d194843";
+  $dbname = "ad_bb6ddb2f34daf52";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * from servicios";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {?>
+      <tr>
+      <td><?php echo $row['nombre_servicio']?></td>
+      <td><?php echo $row['descripcion_servicio']?></td>
+      <td><img src ="<?php echo $row['imagen_servicio']?>"></td>
+      </tr>
+     <?php }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+ ?> 
+ </table>
+      	<div>
+     	<center> 		
+		</center> 
+		</div>	
+	   </center>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+     
+     	<div>
+     	<center> 
+		<image src="https://raw.githubusercontent.com/Kathia-Milagros/ExamenParcial/master/images/12.png" width="1000" height="400" >
+		</center> 
+		</div>		
+    </div>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+    
+ </div>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
 </body>
 </html>
